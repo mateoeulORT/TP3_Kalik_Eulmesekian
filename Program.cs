2371df372bab8058c893ccaa4669769a6e5134be
MATEO EULMESEKIAN <48044168@ort>
2024-05-13 10:27:39 -0300

    ticketera y menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int rta;
+
+
+do
+{
+    Console.WriteLine("1. Nueva Inscricion");
+    Console.WriteLine("2. Obtener estadisticas");
+    Console.WriteLine("3. Buscar cliente");
+    Console.WriteLine("4. Cambiar entrada de un cliente");
+    Console.WriteLine("5. Salir");
+    
+    rta = IngresarMenu("INGRESE UNA OPCION:");
+
+    switch (rta)
+    {
+        
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        default:
+            break;
+    }
+
+} while (rta != 5);
+
+
+
+static int IngresarMenu (string mensaje){
+    int num;
+    do
+    {
+        Console.WriteLine(mensaje);
+        num = int.Parse(Console.ReadLine());
+    } while (num < 0 || num > 5);
+    return num;
+}
+
+
+
+
+
+
+
+
